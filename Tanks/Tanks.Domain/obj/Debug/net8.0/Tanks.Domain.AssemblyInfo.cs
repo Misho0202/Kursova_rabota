1c282430940db5aebc3420dfd53062f7573bc303
Misho0202 <mishi.vachkov@gmail.com>
2025-05-16 00:28:56 +0300

    New

--- Tanks/Tanks.Domain/obj/Debug/net8.0/Tanks.Domain.AssemblyInfo.cs
+++ Tanks/Tanks.Domain/obj/Debug/net8.0/Tanks.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tanks.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c07ad20d1f143f559556f924d9127b97d5ba52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tanks.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tanks.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
